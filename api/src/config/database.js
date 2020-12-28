4a480fb12fd5bb9ae0a4ff8e9291a0891cfa6390
Charllys Castro <charllyscastro@gmail.com>
2020-12-27 22:30:03 -0300

    Conexão do banco de dados com node-postgres

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,1 +1,24 @@
-// conexao a base de dados local com postgres
+/**
+ * Arquivo: src/config/database.js
+ * Descrição: arquivo responsavel pela conection string da aplicação, comunicação da apli. com o postgres.
+ * Data: 27/12/2020
+ * Author: Charllys Castro
+ */
+
+ const { Pool } = require('pg');
+ const dotenv = require('dotenv');
+
+ dotenv.config();
+
+// ==> conexão com a Base de Dados (PostgresSQL)
+ const pool = new Pool({
+   connectionString: process.env.DATABASE_URL
+ });
+
+ pool.on('connect', () =>{
+  console.log('Base de Dados conectado com sucesso!');
+ });
+
+ module.exports = {
+  query: (text, params) => pool.query(text.params)
+ };
